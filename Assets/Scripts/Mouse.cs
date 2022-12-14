9199b20c3eae94330952e1945727627c2b5d20c6
Sedat Doğan <dogannnsedat@gmail.com>
2022-12-14 20:32:45 +0300

    Yukarı aşağı bakış kodu yazıldı ve sınırlandırıldı

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -6,7 +6,7 @@ public class Mouse : MonoBehaviour
 {
     public Transform player;//Player transformu
     float mouseSens = 200f;//Mouse hassasiyeti
-   
+    private float xRotation;
     void Start()
     {
         
@@ -18,6 +18,10 @@ public class Mouse : MonoBehaviour
         float MouseX = Input.GetAxis("Mouse X")*mouseSens*Time.deltaTime;
         float MouseY = Input.GetAxis("Mouse Y")*mouseSens*Time.deltaTime;
         //X ve Y düzleminde mouse girdisini tutacak deðiþkenler.
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);//xRotation deðiþkenini -90 ile 90 deðerleri arasýnda tut.
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         player.Rotate(Vector3.up * MouseX);//Player'ýn rotasyonunu RotateY(Sað sol) düzleminde hareket ettir ve bunu fare ile kontrol et.
     }
