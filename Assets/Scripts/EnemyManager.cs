4938e186a7df313c1f7c9b04a357596f8cce0016
Sedat Doğan <dogannnsedat@gmail.com>
2022-12-23 18:53:54 +0300

    EnemyManager scripti oluşturuldu ve temel kodlar yazıldı

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int enemyHealth = 200;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void EnemyTakeDamage(int damageAmount)
+    {
+        enemyHealth -= damageAmount;
+        if (enemyHealth <= 0)
+        {
+            EnemyHealth();
+        }
+    }
+    void EnemyHealth()
+    {
+        Destroy(this.gameObject);
+
+    }
+}
+
