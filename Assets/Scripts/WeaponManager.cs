2da0a6fbcedc80c7d6faf6f98697bac6fc1db3c3
Sedat Doğan <dogannnsedat@gmail.com>
2022-12-25 19:08:41 +0300

    Ateş etme sesi eklendi

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -8,24 +8,26 @@ public class WeaponManager : MonoBehaviour
     public Camera playerCamera;
     public float range = 300f;
     private EnemyManager enemyManager;//EnemyManager scriptine eriþmek için
-    public ParticleSystem muzzleEffect;
+    public ParticleSystem muzzleEffect;//Namlu ateþi efekti
+    public AudioSource fireSound;//Ateþ etme sesi
     void Start()
     {
-        
+     
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Fire();
-            muzzleEffect.Play();
+            Fire();//Fire metodunu çalýþtýr
+            muzzleEffect.Play();//Namlu ateþ efektini oynat
+            
         }
     }
 
     void Fire()
     {
+        fireSound.Play();//Ateþ etme sesini çalýþtýr
         RaycastHit hit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
         {//playerCamera çýkýþ noktasýndan playerCameranýn forward yönüne doðru range miktarý mesafesinde ýþýn oluþturur.
